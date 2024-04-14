beaff6dbcf2c0505cea980d0b8f83efa270eb920
roxannesyombua <roxannesyombua@gmail.com>
2024-04-14 12:17:37 +0300

    update

--- index.js
+++ index.js
@@ -1,1 +1,79 @@
 // Your code here
+
+function createEmployeeRecord(employeeInfo) {
+    let testEmployee = {
+        firstName: employeeInfo[0],
+        familyName: employeeInfo[1],
+        titleField: employeeInfo[2],
+        payPerHour: employeeInfo[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+
+    return testEmployee;
+}
+
+function createEmployeeRecords(arrayOfArrays) {
+    return arrayOfArrays.map(createEmployeeRecord);
+}
+
+
+function createTimeInEvent(employeeRecord, dateTimeString) {
+    let [date, hour] = dateTimeString.split(' ');
+    employeeRecord.timeInEvents.push({
+        type: "TimeIn",
+        date: date,
+        hour: parseInt(hour)
+    });
+    return employeeRecord;
+}
+
+
+function createTimeOutEvent(employeeRecord, dateTimeString) {
+    let [date, hour] = dateTimeString.split(' ');
+    employeeRecord.timeOutEvents.push({
+        type: "TimeOut",
+        date: date,
+        hour: parseInt(hour)
+    });
+    return employeeRecord;
+}
+
+
+function hoursWorkedOnDate(employeeRecord, date) {
+    let timeInEvent = employeeRecord.timeInEvents.find(event => event.date === date);
+    let timeOutEvent = employeeRecord.timeOutEvents.find(event => event.date === date);
+    return (timeOutEvent.hour - timeInEvent.hour) / 100;
+}
+
+
+function wagesEarnedOnDate(employeeRecord, date) {
+    let hoursWorked = hoursWorkedOnDate(employeeRecord, date);
+    return hoursWorked * employeeRecord.payPerHour;
+}
+
+
+function allWagesFor(employeeRecord) {
+    return employeeRecord.timeInEvents.reduce((totalWages, timeInEvent) => {
+        let date = timeInEvent.date;
+        return totalWages + wagesEarnedOnDate(employeeRecord, date);
+    }, 0);
+}
+
+
+function calculatePayroll(employees) {
+    return employees.reduce((totalPayroll, employee) => {
+        return totalPayroll + allWagesFor(employee);
+    }, 0);
+}
+
+
+const csvDataEmployees = [
+    ["Thor", "Odinsson", "Electrical Engineer", 45],
+    ["Loki", "Laufeysson-Odinsson", "HR Representative", 35],
+    ["Natalia", "Romanov", "CEO", 150],
+    ["Darcey", "Lewis", "Intern", 15],
+    ["Jarvis", "Stark", "CIO", 125],
+    ["Anthony", "Stark", "Angel Investor", 300]
+];
+
